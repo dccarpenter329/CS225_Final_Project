b9b9b3d57189cb8296910e28cba88a08c63f9dd2
dccarpenter329 <dccarpenter329@gmail.com>
2025-11-10 23:50:36 -0700

    Update Gringotts.h

--- Gringotts.h
+++ Gringotts.h
@@ -1,24 +1,49 @@
 #include <iostream>
+using namespace std;
+void wand();
+void wandQuiz();
+int error(int);
+
+//Global Variables
+int wand;
+//void cost(int, int, int, BankAccount&); 
+//void bank(Gringotts&);
 class Wallet {
 protected:
 	int walletGalleons;
 	int walletSickles;
 	int walletKnuts;
-//	int walletBalance;
 public: 
     void walletBalance();
 };
-
-class Gringotts: public Wallet {
-private:
-	int Galleons;
+class BankAccount {
+protected: 
+  string Keyword;
+  string Name;
+ 	int Galleons;
 	int Sickles;
 	int Knuts;
 	int Balance;
 public:
-	Gringotts();
+  BankAccount(string k, string n) {Keyword=k; Name=n;}
+  friend ostream& operator<<(ostream& out, BankAccount& temp);
+  int setGalleons(int G) {Galleons=G;}
+  int setSickles(int S) {Sickles=S;}
+  int setKnuts(int K) {Knuts=K;}
+  int& getGalleons() {return Galleons;}
+  int& getSickles() {return Sickles;}
+  int& getKnuts() {return Knuts;}
+  string& getName() {return Name;}
+};
+
+class Gringotts: public Wallet {
+private:
+  BankAccount bankAccount;
+public:
+	Gringotts(string,string);
+  void transaction();
 	void balance();
 	void withdraw();
 	void deposit();
-	void bank();
+	//void bank(Gringotts&);
 };

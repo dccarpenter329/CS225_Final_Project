1e1295625546e7d961a3e249f92b48c7f5fdaa05
dccarpenter329 <dccarpenter329@gmail.com>
2025-11-18 00:49:46 -0700

    Create SpellCasting.cpp

---
+++ SpellCasting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Gringotts.h"
+
+string SpellCasting() {
+	string s;
+	/*Exception error message*/
+	const string MSG= "The spell backfires on yourself and you go unconscious.";
+	cout << "At home, with all your supplies you can now cast your first spell!" << endl;
+	cout << "Enter your first spell: ";
+	cin.ignore(200,'\n');
+	getline(cin, s, '\n'); // Read in spell
+	/* Exception throw block*/
+	if (s== "stupefy" || s== "Stupefy" )
+		throw(MSG); // Throw an error message to catch block
+	return s; // Return the spell string
+}
